c3a118f87703f32010ed2126c0581006128804e6
aliifam <aliif.arief.m@gmail.com>
2023-05-02 07:45:22 +0700

    done all features

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,7 @@
         <v-select
             label="Jenis Jadwal"
             :items="['Per - Hari', 'Per - Bulan']"
+            v-model="jenis"
         ></v-select>
 
         <v-autocomplete
@@ -30,6 +31,66 @@
         <v-btn block size="large" @click="generateJadwal"
             >Generate Jadwal</v-btn
         >
+
+        <br />
+
+        <v-card v-if="jadwal && jenis == 'Per - Hari'">
+            <v-card-title>
+                <h2>Jadwal Sholat Harian {{ jadwal.data.lokasi }}</h2>
+            </v-card-title>
+            <v-card-text>
+                <v-simple-table>
+                    <template v-slot:default>
+                        <p>Lokasi : {{ jadwal.data.lokasi }}</p>
+                        <p>Tanggal : {{ jadwal.data.jadwal.tanggal }}</p>
+
+                        <v-divider></v-divider>
+
+                        <p>Subuh : {{ jadwal.data.jadwal.subuh }}</p>
+                        <p>Dzuhur : {{ jadwal.data.jadwal.dzuhur }}</p>
+                        <p>Ashar : {{ jadwal.data.jadwal.ashar }}</p>
+                        <p>Maghrib : {{ jadwal.data.jadwal.maghrib }}</p>
+                        <p>Isya : {{ jadwal.data.jadwal.isya }}</p>
+
+                        <v-divider></v-divider>
+
+                        <p>Imsak : {{ jadwal.data.jadwal.imsak }}</p>
+                        <p>Terbit : {{ jadwal.data.jadwal.terbit }}</p>
+                        <p>Dhuha : {{ jadwal.data.jadwal.dhuha }}</p>
+                    </template>
+                </v-simple-table>
+            </v-card-text>
+        </v-card>
+
+        <v-card v-if="jadwal && jenis == 'Per - Bulan'">
+            <v-card-title>
+                <h2>Jadwal Sholat Bulanan {{ jadwal.data.lokasi }}</h2>
+            </v-card-title>
+
+            <v-card-text v-for="item in jadwal.data.jadwal" :key="item">
+                <v-simple-table>
+                    <template v-slot:default>
+                        <p>Tanggal : {{ item.tanggal }}</p>
+
+                        <v-divider></v-divider>
+
+                        <p>Subuh : {{ item.subuh }}</p>
+                        <p>Dzuhur : {{ item.dzuhur }}</p>
+                        <p>Ashar : {{ item.ashar }}</p>
+                        <p>Maghrib : {{ item.maghrib }}</p>
+                        <p>Isya : {{ item.isya }}</p>
+
+                        <v-divider></v-divider>
+
+                        <p>Imsak : {{ item.imsak }}</p>
+                        <p>Terbit : {{ item.terbit }}</p>
+                        <p>Dhuha : {{ item.dhuha }}</p>
+                    </template>
+                </v-simple-table>
+            </v-card-text>
+        </v-card>
+
+        <br />
     </v-container>
 </template>
 
@@ -48,6 +109,7 @@ export default {
             allcityandcode: [],
             nowdate: null,
             nowmonth: null,
+            nowyear: null,
         };
     },
     methods: {
@@ -84,9 +146,43 @@ export default {
         },
 
         generateJadwal() {
-            this.nowmonth = new Date().getMonth() + 1;
             this.nowdate = new Date().getDate();
-            console.log(this.nowdate);
+            this.nowmonth = new Date().getMonth() + 1;
+            this.nowyear = new Date().getFullYear();
+
+            if (this.jenis == "Per - Hari") {
+                this.fetchJadwalHarian();
+            } else {
+                this.fetchJadwalBulanan();
+            }
+        },
+
+        async fetchJadwalHarian() {
+            const Jadwal = await axios.get(
+                "https://api.myquran.com/v1/sholat/jadwal/" +
+                    this.kota.id +
+                    "/" +
+                    this.nowyear +
+                    "/" +
+                    this.nowmonth +
+                    "/" +
+                    this.nowdate
+            );
+            this.jadwal = Jadwal.data;
+            console.log(this.jadwal.data);
+        },
+
+        async fetchJadwalBulanan() {
+            const Jadwal = await axios.get(
+                "https://api.myquran.com/v1/sholat/jadwal/" +
+                    this.kota.id +
+                    "/" +
+                    this.nowyear +
+                    "/" +
+                    this.nowmonth
+            );
+            this.jadwal = Jadwal.data;
+            console.log(this.jadwal.data);
         },
     },
     async mounted() {
